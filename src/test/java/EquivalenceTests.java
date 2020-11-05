5147bcbde1e9f34f9457e987eebabec22f793143
BrianM0330 <BrianM0330@gmail.com>
2020-11-04 22:34:37 -0600

    Equivalence Partitioning Done

--- src/test/java/EquivalenceTests.java
+++ src/test/java/EquivalenceTests.java
@@ -3,6 +3,7 @@ import edu.depaul.se433.shoppingapp.TotalCostCalculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.BeforeEach;
 
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -14,34 +15,79 @@ public class EquivalenceTests {
 
     private TotalCostCalculator calculator;
 
-    @BeforeEach
-    void setup() {
-        calculator = new TotalCostCalculator();
-    }
-
     @ParameterizedTest
     @MethodSource("weakNormalValues")
     @DisplayName("Weak Normal Equivalence Tests")
-    void testWeakNormal() {
+    void testWeakNormal(String state, ShippingType shipType, Double rawPrice, Double expectedTotal) {
+        Double result = TotalCostCalculator.calculate(rawPrice, state, shipType);
+        assertEquals(expectedTotal, result);
+    }
 
+    @ParameterizedTest
+    @MethodSource("weakRobustValues")
+    @DisplayName("Weak Robust Equivalence Tests")
+    void testWeakRobust(String state, ShippingType shipType, Double rawPrice, Double expectedTotal) {
+        if (rawPrice == 0.0 || state.equals("")) {
+            assertThrows(IllegalArgumentException.class, () -> TotalCostCalculator.calculate(rawPrice, state, shipType));
+        }
+
+        else if (shipType == null) {
+            assertThrows(NullPointerException.class, () -> TotalCostCalculator.calculate(rawPrice, state, shipType));
+        }
+
+        else {
+            Double result = TotalCostCalculator.calculate(rawPrice, state, shipType);
+            assertEquals(expectedTotal, result);
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("strongNormalValues")
+    @DisplayName("Strong Normal Equivalence Tests")
+    void testStrongNormal(String state, ShippingType shipType, Double rawPrice, Double expectedTotal) {
+        Double result = TotalCostCalculator.calculate(rawPrice, state, shipType);
+        assertEquals(expectedTotal, result);
+    }
+
+    @ParameterizedTest
+    @MethodSource("strongRobustValues")
+    @DisplayName("Strong Robust Equivalence Tests")
+    void testStrongRobust(String state, ShippingType shipType, Double rawPrice, Double expectedTotal) {
+    //NullPointer only on null shipType, IllegalArgs on the other two. Just want to make sure that tests fail properly
+        if (rawPrice == 0.0 ) {
+            assertThrows(IllegalArgumentException.class, () ->TotalCostCalculator.calculate(rawPrice, state, shipType));
+        }
+
+        else if (shipType == null) {
+            assertThrows(NullPointerException.class, () ->TotalCostCalculator.calculate(rawPrice, state, shipType));
+        }
+
+        else if (state.equals("")) {
+            assertThrows(IllegalArgumentException.class, () -> TotalCostCalculator.calculate(rawPrice, state, shipType));
+        }
+
+        else {
+            Double result = TotalCostCalculator.calculate(rawPrice, state, shipType);
+            assertEquals(expectedTotal, result);
+        }
     }
 
     private static Stream<Arguments> weakNormalValues () {
         return Stream.of(
             Arguments.of("IL", ShippingType.STANDARD, 25.00, 36.5),
-            Arguments.of("AK", ShippingType.NEXT_DAY, 75.00, 100)
+            Arguments.of("AK", ShippingType.NEXT_DAY, 75.00, 100.0)
         );
     }
 
     private static Stream<Arguments> weakRobustValues () {
         return Stream.of(
             Arguments.of("IL", ShippingType.STANDARD, 25.00, 36.5),
-            Arguments.of("AK", ShippingType.NEXT_DAY, 75.00, 100),
+            Arguments.of("AK", ShippingType.NEXT_DAY, 75.00, 100.0),
 
             //Invalid cases
-            Arguments.of("", ShippingType.STANDARD, 25.00, -1),
-            Arguments.of("IL", null, 25.00, -1),
-            Arguments.of("IL", ShippingType.STANDARD, 0, -1)
+            Arguments.of("", ShippingType.STANDARD, 25.00, -1.0),
+            Arguments.of("IL", null, 25.00, -1.0),
+            Arguments.of("IL", ShippingType.STANDARD, 0.0, -1.0)
         );
     }
 
@@ -76,20 +122,20 @@ public class EquivalenceTests {
             Arguments.of("AK", ShippingType.NEXT_DAY, 75.00, 100.00),
 
             //Invalid Cases
-            Arguments.of("", ShippingType.STANDARD, 25.00, -1),
-            Arguments.of("", ShippingType.STANDARD, 75.00, -1),
-            Arguments.of("", ShippingType.NEXT_DAY, 25.00, -1),
-            Arguments.of("", ShippingType.NEXT_DAY, 75.00, -1),
+            Arguments.of("", ShippingType.STANDARD, 25.00, -1.0),
+            Arguments.of("", ShippingType.STANDARD, 75.00, -1.0),
+            Arguments.of("", ShippingType.NEXT_DAY, 25.00, -1.0),
+            Arguments.of("", ShippingType.NEXT_DAY, 75.00, -1.0),
 
-            Arguments.of("IL", null, 25.00, -1),
-            Arguments.of("IL", null, 75.00, -1),
-            Arguments.of("AK", null, 25.00, -1),
-            Arguments.of("AK", null, 75.00, -1),
+            Arguments.of("IL", null, 25.00, -1.0),
+            Arguments.of("IL", null, 75.00, -1.0),
+            Arguments.of("AK", null, 25.00, -1.0),
+            Arguments.of("AK", null, 75.00, -1.0),
 
-            Arguments.of("IL", ShippingType.STANDARD, 0, -1),
-            Arguments.of("IL", ShippingType.NEXT_DAY, 0, -1),
-            Arguments.of("AK", ShippingType.STANDARD, 0, -1),
-            Arguments.of("AK", ShippingType.NEXT_DAY, 0, -1)
+            Arguments.of("IL", ShippingType.STANDARD, 0.0, -1.0),
+            Arguments.of("IL", ShippingType.NEXT_DAY, 0.0, -1.0),
+            Arguments.of("AK", ShippingType.STANDARD, 0.0, -1.0),
+            Arguments.of("AK", ShippingType.NEXT_DAY, 0.0, -1.0)
         );
     }
 }
